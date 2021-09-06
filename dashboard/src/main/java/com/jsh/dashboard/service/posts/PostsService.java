23ff2410607a63497dbb12316745f99f392f885a
josunghyeon <61929745+josh5734@users.noreply.github.com>
2021-09-06 21:46:03 +0900

    Create Posts & View all the posts

--- dashboard/src/main/java/com/jsh/dashboard/service/posts/PostsService.java
+++ dashboard/src/main/java/com/jsh/dashboard/service/posts/PostsService.java
@@ -3,13 +3,16 @@ package com.jsh.dashboard.service.posts;
 
 import com.jsh.dashboard.domain.posts.Posts;
 import com.jsh.dashboard.domain.posts.PostsRepository;
+import com.jsh.dashboard.web.dto.PostsListResponseDto;
 import com.jsh.dashboard.web.dto.PostsResponseDto;
 import com.jsh.dashboard.web.dto.PostsSaveRequestDto;
 import com.jsh.dashboard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,7 +38,14 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
---
+++ dashboard/src/test/java/com/jsh/dashboard/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.jsh.dashboard.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지_로딩(){
+        String body = this.restTemplate.getForObject("/", String.class);
+        System.out.println("body = " + body);
+        assertThat(body).contains("Community");
+
+    }
+}
