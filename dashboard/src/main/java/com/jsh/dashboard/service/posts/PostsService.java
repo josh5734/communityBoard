95abf643fc5793c714b2f8abdbc5c9e7e937b075
josunghyeon <61929745+josh5734@users.noreply.github.com>
2021-09-08 11:34:36 +0900

    Add delete Service Logic

--- dashboard/src/main/java/com/jsh/dashboard/service/posts/PostsService.java
+++ dashboard/src/main/java/com/jsh/dashboard/service/posts/PostsService.java
@@ -20,27 +20,29 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    // 등록
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    // 수정
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
-
         posts.update(requestDto.getTitle(), requestDto.getContent());
-
         return id;
 
     }
 
+    // 조회
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
     }
 
+    // 모든 포스트 조회
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream()
@@ -48,4 +50,11 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    // 삭제
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
 }
