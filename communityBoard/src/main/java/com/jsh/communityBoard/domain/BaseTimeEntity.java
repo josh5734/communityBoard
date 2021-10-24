f9f16cb3725b677b6a9e3195ffb47825d009157c
josunghyeon <61929745+josh5734@users.noreply.github.com>
2021-10-24 22:25:38 +0900

    Datetime Formatting

--- communityBoard/src/main/java/com/jsh/communityBoard/domain/BaseTimeEntity.java
+++ communityBoard/src/main/java/com/jsh/communityBoard/domain/BaseTimeEntity.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -18,5 +20,15 @@ public class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
-
+//
+//    @PrePersist
+//    public void onPrePersist(){
+//        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+//        this.modifiedDate = this.createdDate;
+//    }
+//
+//    @PreUpdate
+//    public void onPreUpdate(){
+//        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+//    }
 }
